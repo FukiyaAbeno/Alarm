e12bbe3a203439d7b2929bfd33e8b679ea97aee7
yukkyo <yukkyo.ng@gmail.com>
2025-12-13 19:17:40 +0900

    30秒経過音声のスタートタイミングを変更

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 let countdownInterval;
 let Duration = 180;
-let AlartSec = 32;  // 2秒手前から警告音声をスタートさせる
+let AlartSec = 31;  // 1秒手前から警告音声をスタートさせる
 let startTimestamp = null;
 let remaining = Duration;
 let elapsed = 0;
